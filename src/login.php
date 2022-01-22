3d8dc26d7990d9bc18443b086f39b4357097b4a3
ashababnoor <ashababnoor@gmail.com>
2022-01-23 04:53:08 +0600

    added new signup confirmation in login

--- src/login.php
+++ src/login.php
@@ -97,7 +97,28 @@ if(
         
         <!-- Main Section -->
         <main class="my-5 editor-bg text-dark text-center" style="min-height: 100vh;">
+
         <div class="form-signin">
+        <?php
+            if(isset($_SESSION['justloggedin']) && $_SESSION['justloggedin'] == true){
+                $_SESSION['justloggedin'] = false;
+                ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                        </symbol>
+                    </svg>
+
+                    <div class="alert alert-success alert-dismissible fade show mb-5" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                        <strong>Hurray!</strong> You have successfully signed up.
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php
+            }
+
+        ?>
+
         <form action="loginProcess.php" method="post" >
             <div class="d-flex align-items-center justify-content-center mb-3" style="gap: 1em;">
                 <img class="mb-2" src="../assets/images/scp-logo-green.svg" alt="" width="72" height="57">
